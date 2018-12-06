557462e92ac1f53c94a63ffb7bd597abfb1cf71d
Tejasree V <tejasree.vangapelli@gmail.com>
2018-12-05 18:37:57 -0600

    Initial Commit

---
+++ GradProjectFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradProjectFinal
+//
+//  Created by OSU App Center on 12/5/18.
+//  Copyright © 2018 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
